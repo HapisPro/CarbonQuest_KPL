7c6d5856b83812530aec67397b911c878aa4d405
alkausarhapis <alkausarhapis@gmail.com>
2025-05-07 17:43:13 +0700

    fix: cross origin bug

---
+++ CarbonQuest.API/Models/LeaderboardEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace CarbonQuest.Main.Models
+{
+    public class LeaderboardEntry
+    {
+        public string Username { get; set; }
+        public int Score { get; set; }
+    }
+}
--- CarbonQuest.API/Program.cs
+++ CarbonQuest.API/Program.cs
@@ -1,20 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
-// Configure
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseCors();
+
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
